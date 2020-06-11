d47437abd6c738fd9034c78e19c3932ddae676fe
Lucien Najev <luciennajev@hotmail.com>
2020-06-11 13:03:39 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Test from './components/Test';
+
+function App() {
+  return (
+    <div>
+      <h1>Insta clone front end</h1>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders learn react link', () => {
+  const wrapper = shallow(<App />);
+  wrapper.debug();
+});
